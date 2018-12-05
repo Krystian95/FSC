564ae0a2f460b4e51e9dcdc6819f2d778dfcaf4d
LLanzarone <30932371+LLanzarone@users.noreply.github.com>
2018-12-05 17:30:08 +0100

    Layout bottoni e altri elementi.
    Layout bottoni e altri elementi.

--- js/index.js
+++ js/index.js
@@ -13,6 +13,26 @@ $(function () {
      }
      });*/
     $(document).on('input change', 'input[name="popolazione_slider"]', function () {
-        $('input[name="popolazione_text"]').val($(this).val());
+        $('input[name="popolazione_textbox"]').val($(this).val());
+    });
+    
+    $(document).on('input change', 'input[name="popolazione_textbox"]', function () {
+        $('input[name="popolazione_slider"]').val($(this).val());
+    });
+    
+    $(document).on('input change', 'input[name="ricchezza_slider"]', function () {
+        $('input[name="ricchezza_textbox"]').val($(this).val());
+    });
+    
+    $(document).on('input change', 'input[name="ricchezza_textbox"]', function () {
+        $('input[name="ricchezza_slider"]').val($(this).val());
+    });
+    
+    $(document).on('input change', 'input[name="salute_slider"]', function () {
+        $('input[name="salute_textbox"]').val($(this).val());
+    });
+    
+    $(document).on('input change', 'input[name="salute_textbox"]', function () {
+        $('input[name="salute_slider"]').val($(this).val());
     });
 });
