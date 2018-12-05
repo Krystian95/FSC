6b16096f578716183ea2ae23b7940c6de8bd31fc
LLanzarone <30932371+LLanzarone@users.noreply.github.com>
2018-12-05 18:40:38 +0100

    Piccole modifiche.
    Piccole modifiche.

--- index.php
+++ index.php
@@ -121,22 +121,19 @@
                 <div class="col">
                     <div class="row align-items-center">
                         <div class="col">
-                            <form>
+                            <form id="params-form" method="post" action="#!">
                                 <div class="row form-group align-items-center">
                                     <label class="col-form-label col-2">
                                         Popolazione
                                     </label>
                                     <div class="col-3">
-                                        <!--<div id="slider">
-                                            <div id="custom-handle" class="ui-slider-handle"></div>
-                                        </div>-->
                                         <input type="range" class="custom-range" min="10" max="5000" step="1" name="popolazione_slider" value="1000">
                                     </div>
                                     <div class="col-3">
                                         <input type="number" class="form-control textbox" min="10" max="5000" step="1" name="popolazione_textbox" value="1000"> 
                                     </div>
                                     <div class="col">
-                                        <button type="button" class="btn btn-light">Imposta altri parametri</button>
+                                        <button type="button" class="btn btn-light">Imposta parametri</button>
                                     </div>
                                 </div>
                                 <div class="row form-group align-items-center">
@@ -153,7 +150,7 @@
                                         <input type="number" class="form-control textbox" min="10" max="5000" step="1" name="ricchezza_textbox" value="1000"> 
                                     </div>
                                     <div class="col">
-                                    <button type="button" class="btn btn-success">Start</button>
+                                    <button type="submit" class="btn btn-success">Start</button>
                                 </div> 
                                 </div>
                                 <div class="row form-group align-items-center">
@@ -195,13 +192,15 @@
                         </label>
                         <div class="col">
                             <div class="progress">
-                                <div class="progress-bar progress-bar-striped progress-bar-animated" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
+                                <div class="progress-bar progress-bar-striped" role="progressbar" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100" style="width: 75%"></div>
                             </div>
                         </div>
                     </div>
                 </div>
             </div>    
         </div>
+        
+        <canvas id="myChart" width="100" height="100"></canvas>
 
         <!-- Footer -->
         <footer class="py-5 bg-dark">
@@ -216,6 +215,7 @@
         <script src="js/lib/jquery-ui.min.js"></script>
         <script src="js/lib/popper-1.14.3.min.js"></script>
         <script src="js/lib/bootstrap-4.1.3.min.js"></script>
+        <script src="js/lib/Chart.bundle.min.js"></script>
         <script src="js/index.js"></script>
 
     </body>
--- js/index.js
+++ js/index.js
@@ -1,38 +1,83 @@
 
-$(function () {
-    var handle = $("#custom-handle");
+function initChart() {
 
-    // http://api.jqueryui.com/slider/
-    /*$("#slider").slider({
-     max: 10000,
-     create: function () {
-     handle.text($(this).slider("value"));
-     },
-     slide: function (event, ui) {
-     handle.text(ui.value);
-     }
-     });*/
+    var ctx = document.getElementById("myChart").getContext('2d');
+    
+    var myChart = new Chart(ctx, {
+        type: 'line',
+        data: {
+            labels: ["Red", "Blue", "Yellow 66", "Green", "Purple", "Orange"],
+            datasets: [{
+                    label: '# of Votes',
+                    data: [12, 19, 3, 5, 2, 3],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                }]
+        },
+        options: {
+            scales: {
+                yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+            }
+        }
+    });
+}
+
+$(function () {
+
+    /* Popolazione */
+
     $(document).on('input change', 'input[name="popolazione_slider"]', function () {
         $('input[name="popolazione_textbox"]').val($(this).val());
     });
-    
+
     $(document).on('input change', 'input[name="popolazione_textbox"]', function () {
         $('input[name="popolazione_slider"]').val($(this).val());
     });
-    
+
+    /* Ricchezza */
+
     $(document).on('input change', 'input[name="ricchezza_slider"]', function () {
         $('input[name="ricchezza_textbox"]').val($(this).val());
     });
-    
+
     $(document).on('input change', 'input[name="ricchezza_textbox"]', function () {
         $('input[name="ricchezza_slider"]').val($(this).val());
     });
-    
+
+    /* Salute */
+
     $(document).on('input change', 'input[name="salute_slider"]', function () {
         $('input[name="salute_textbox"]').val($(this).val());
     });
-    
+
     $(document).on('input change', 'input[name="salute_textbox"]', function () {
         $('input[name="salute_slider"]').val($(this).val());
     });
+
+    /* Anima slider */
+
+    $("#params-form").submit(function (event) {
+        $('.progress-bar').addClass('progress-bar-animated');
+    });
+
+    initChart();
 });
